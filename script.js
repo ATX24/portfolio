92a63f26d85d97c952551f5a67c7646c982562a0
ATX24 <87243784+ATX24@users.noreply.github.com>
2023-03-26 22:09:13 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// Typing animation for the titles
+const titles = ['Dhilan Shah', 'NASA Intern', 'Software Developer', 'Web Designer', 'Problem Solver'];
+const titleElement = document.querySelector('h1');
+let titleIndex = 0;
+let charIndex = 0;
+let deleteMode = false;
+let timeoutId = null;
+
+function typeTitle() {
+  const currentTitle = titles[titleIndex];
+  if (deleteMode) {
+    titleElement.textContent = currentTitle.substring(0, charIndex - 1);
+    charIndex--;
+    if (charIndex === 0) {
+      deleteMode = false;
+      titleIndex++;
+      if (titleIndex === titles.length) {
+        titleIndex = 0;
+      }
+    }
+  } else {
+    titleElement.textContent = currentTitle.substring(0, charIndex + 1);
+    charIndex++;
+    if (charIndex === currentTitle.length) {
+      deleteMode = true;
+      timeoutId = setTimeout(typeTitle, 1000);
+    }
+  }
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(typeTitle, 150);
+}
+
+typeTitle();
+
+
+
+
